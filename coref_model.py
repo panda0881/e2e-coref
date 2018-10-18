1b776af64b98288affb76f9482da79ffa6bc92d0
hongmingzhang <hzhangal0330@gmail.com>
2018-10-18 20:15:54 +0800

    compute OMCS coverage

--- coref_model.py
+++ coref_model.py
@@ -306,12 +306,11 @@ class CorefModel(object):
         flattened_sentence_indices = self.flatten_emb_by_sentence(sentence_indices, text_len_mask)  # [num_words]
         flattened_head_emb = self.flatten_emb_by_sentence(head_emb, text_len_mask)  # [num_words]
 
-        # candidate_starts = tf.tile(tf.expand_dims(tf.range(num_words), 1),
-        #                            [1, self.max_span_width])  # [num_words, max_span_width]
-        # candidate_ends = candidate_starts + tf.expand_dims(tf.range(self.max_span_width),
-        #                                                    0)  # [num_words, max_span_width]
-        candidate_starts = gold_starts
-        candidate_ends = gold_ends
+        candidate_starts = tf.tile(tf.expand_dims(tf.range(num_words), 1),
+                                   [1, self.max_span_width])  # [num_words, max_span_width]
+        candidate_ends = candidate_starts + tf.expand_dims(tf.range(self.max_span_width),
+                                                           0)  # [num_words, max_span_width]
+
         candidate_start_sentence_indices = tf.gather(flattened_sentence_indices,
                                                      candidate_starts)  # [num_words, max_span_width]
         candidate_end_sentence_indices = tf.gather(flattened_sentence_indices, tf.minimum(candidate_ends,
@@ -319,9 +318,11 @@ class CorefModel(object):
         candidate_mask = tf.logical_and(candidate_ends < num_words, tf.equal(candidate_start_sentence_indices,
                                                                              candidate_end_sentence_indices))  # [num_words, max_span_width]
         flattened_candidate_mask = tf.reshape(candidate_mask, [-1])  # [num_words * max_span_width]
-        candidate_starts = tf.boolean_mask(tf.reshape(candidate_starts, [-1]),
-                                           flattened_candidate_mask)  # [num_candidates]
-        candidate_ends = tf.boolean_mask(tf.reshape(candidate_ends, [-1]), flattened_candidate_mask)  # [num_candidates]
+        # candidate_starts = tf.boolean_mask(tf.reshape(candidate_starts, [-1]),
+        #                                    flattened_candidate_mask)  # [num_candidates]
+        # candidate_ends = tf.boolean_mask(tf.reshape(candidate_ends, [-1]), flattened_candidate_mask)  # [num_candidates]
+        candidate_starts = gold_starts
+        candidate_ends = gold_ends
 
         candidate_sentence_indices = tf.boolean_mask(tf.reshape(candidate_start_sentence_indices, [-1]),
                                                      flattened_candidate_mask)  # [num_candidates]
