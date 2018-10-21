1f69eaa8fb2e244d9ee01a86e14589b8f5fe9d0a
hongmingzhang <hzhangal0330@gmail.com>
2018-10-21 15:09:20 +0800

    pronoun coreference

--- ner_detection.py
+++ ner_detection.py
@@ -9,9 +9,14 @@ nlp_list = [StanfordCoreNLP('http://localhost:900%d' % (i)) for i in range(no_nl
 
 tmp_nlp = nlp_list[0]
 
+all_examples = list()
 with open('test.english.jsonlines', 'r') as f:
+    counter = 0
     for line in f:
+        counter += 1
+        print(counter)
         tmp_example = json.loads(line)
+        tmp_example['parsed_result'] = list()
         for w_list in tmp_example['sentences']:
             tmp_s = ''
             for w in w_list:
@@ -21,6 +26,10 @@ with open('test.english.jsonlines', 'r') as f:
                 tmp_s = tmp_s[1:]
                 tmp_output = tmp_nlp.annotate(tmp_s,
                                               properties={'annotators': 'tokenize,pos,lemma,ner', 'outputFormat': 'json'})
-                print(tmp_output)
+                tmp_example['parsed_result'].append(tmp_output)
+        all_examples.append(tmp_example)
+
+with open('parsed_test_example.json', 'w') as f:
+    json.dump(all_examples, f)
 
 print('end')
