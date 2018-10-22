d02fde3a55e54690f0cb0cd20032cf835a51aa76
hongmingzhang <hzhangal0330@gmail.com>
2018-10-22 20:05:26 +0800

    pronoun coreference

--- failed_case_analyze.py
+++ failed_case_analyze.py
@@ -3,6 +3,8 @@
 # from util import *
 import ujson as json
 from pycorenlp import StanfordCoreNLP
+from multiprocessing import Pool
+from tqdm import tqdm
 
 interested_entity_types = ['NATIONALITY', 'ORGANIZATION', 'PERSON', 'DATE', 'CAUSE_OF_DEATH', 'CITY', 'LOCATION',
                            'NUMBER', 'TITLE', 'TIME', 'ORDINAL', 'DURATION', 'MISC', 'COUNTRY', 'SET', 'PERCENT',
@@ -85,6 +87,7 @@ with open('failed_cases.jsonlines', 'r') as f:
         tmp_failed_cases = json.loads(line)
         all_failed_cases.append(tmp_failed_cases)
 
+all_data_for_analyze = list()
 for i in range(len(all_test_examples)):
     print('We are working on example:', i)
     if len(all_failed_cases[i]) == 0:
@@ -138,7 +141,171 @@ for i in range(len(all_test_examples)):
             Before_length += len(s['tokens'])
         # print(len(stored_dependency_list))
         tmp_data_to_analyze.append({'NP': (NP_position, NP), 'pronoun_related_edge': stored_dependency_list})
-    print('lalala')
+    all_data_for_analyze.append(tmp_data_to_analyze)
+with open('test_data_for_analyzing.json', 'w') as f:
+    json.dump(all_data_for_analyze, f)
 
-print(all_entity_type)
-print('end')
+stop_words = list()
+with open('nltk_english.txt', 'r') as f:
+    for line in f:
+        stop_words.append(line[:-1])
+stop_words = set(stop_words)
+
+with open('test_data_for_analyzing.json', 'r') as f:
+    all_test_data = json.load(f)
+
+OMCS_data = list()
+with open('OMCS/new_omcs600.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        words = line.split('\t')
+        OMCS_data.append((words[0], words[1].split(' '), words[2].split(' ')))
+
+
+def filter_stop_words(input_sentence, stop_words):
+    result = list()
+    for w in input_sentence:
+        if w in stop_words:
+            continue
+        result.append(w)
+    return result
+
+
+def get_coverage(w_list1, w_list2):
+    if len(w_list1) == 0:
+        return 0
+    tmp_count = 0
+    for w in w_list1:
+        if w in w_list2:
+            tmp_count += 1
+    # if tmp_count > 0:
+    #     print('')
+    return tmp_count / len(w_list1)
+
+
+def verify_match(coreference_pair, OMCS_pair, limitation=0.5):
+    if get_coverage(coreference_pair[0], OMCS_pair[0]) >= limitation and get_coverage(coreference_pair[1],
+                                                                                      OMCS_pair[1]) >= limitation:
+        return True
+    if get_coverage(coreference_pair[0], OMCS_pair[1]) >= limitation and get_coverage(coreference_pair[1],
+                                                                                      OMCS_pair[0]) >= limitation:
+        return True
+    return False
+
+
+def find_OMCS_match_for_a_coreference_pair(tmp_data, example_id):
+    print('We are working on example:', example_id, '/', 348)
+    found_match_pair = 0
+    if len(tmp_data) == 0:
+        print('This example has no valid data')
+        return 0, 0
+    for NP_P_pair in tmp_data:
+        NP = NP_P_pair['NP'][1]
+        for edge in NP_P_pair['pronoun_related_edge']:
+            if edge[0][1] in all_pronouns:
+                tmp_other_word = edge[2][1]
+            else:
+                tmp_other_word = edge[0][1]
+            found_match = False
+            for pair in OMCS_data:
+                if tmp_other_word not in stop_words and verify_match((filter_stop_words(NP, stop_words), [tmp_other_word]), pair[1:]):
+                    found_match = True
+                    break
+            if found_match:
+                found_match_pair += 1
+                break
+    print('File', example_id, 'Found_match:', found_match_pair, '/', len(tmp_data), found_match_pair / len(tmp_data))
+    return found_match_pair, len(tmp_data)
+
+
+def get_match_dict(tmp_data, example_id):
+    print('We are working on example:', example_id, '/', 348)
+    local_dict = dict()
+    for edge in OMCS_edges:
+        local_dict[edge] = dict()
+    if len(tmp_data) == 0:
+        print('This example has no valid data')
+        return local_dict
+    for NP_P_pair in tmp_data:
+        NP = NP_P_pair['NP'][1]
+        for edge in NP_P_pair['pronoun_related_edge']:
+            if edge[0][1] in all_pronouns:
+                tmp_other_word = edge[2][1]
+            else:
+                tmp_other_word = edge[0][1]
+            for pair in OMCS_data:
+                if verify_match((NP, tmp_other_word), pair[1:]) and pair[0] in OMCS_edges:
+                    if edge[1] not in local_dict[pair[0]]:
+                        local_dict[pair[0]][edge[1]] = 0
+                    local_dict[pair[0]][edge[1]] += 1
+    print('File', example_id, 'finished')
+    return local_dict
+
+
+OMCS_edges = ['AtLocation', 'UsedFor', 'IsA', 'CapableOf', 'HasPrerequisite', 'HasProperty', 'Causes', 'HasA',
+              'MotivatedByGoal', 'Desires', 'CausesDesire', 'PartOf', 'ReceivesAction', 'MadeOf', 'DefinedAs',
+              'HasFirstSubevent', 'HasLastSubevent', 'RelatedTo', 'CreatedBy', 'SymbolOf', 'InstanceOf', 'HasSubevent',
+              'InheritsFrom', 'LocatedNear', 'HasPainIntensity', 'HasPainCharacter', 'DesireOf', 'LocationOfAction']
+
+match_result = dict()
+
+example_and_ids = list()
+for i, tmp_data in enumerate(all_test_data):
+    example_and_ids.append((tmp_data, i))
+
+workers = Pool(30)
+
+
+
+# raw_results = list()
+# for example_and_id in example_and_ids:
+#     tmp_result = workers.apply_async(find_OMCS_match_for_a_coreference_pair,
+#                                      args=(example_and_id[0], example_and_id[1],))
+#     raw_results.append(tmp_result)
+# workers.close()
+# workers.join()
+# raw_results = [tmp_result.get() for tmp_result in raw_results]
+#
+# all_matched_pairs = 0
+# all_pairs = 0
+#
+# for p in raw_results:
+#     all_matched_pairs += p[0]
+#     all_pairs += p[1]
+#
+# print(all_matched_pairs, all_pairs, all_matched_pairs / all_pairs)
+
+
+raw_dicts = list()
+for example_and_id in example_and_ids:
+    tmp_dict = workers.apply_async(get_match_dict, args=(example_and_id[0], example_and_id[1],))
+    raw_dicts.append(tmp_dict)
+workers.close()
+workers.join()
+raw_dicts = [tmp_dict.get() for tmp_dict in raw_dicts]
+
+
+print('Start to merge dict')
+final_dict = dict()
+for edge in OMCS_edges:
+    final_dict[edge] = dict()
+
+for tmp_dict in tqdm(raw_dicts):
+    try:
+        for edge in OMCS_edges:
+            for dep_edge in tmp_dict[edge]:
+                if dep_edge not in final_dict[edge]:
+                    final_dict[edge][dep_edge] = 0
+                final_dict[edge][dep_edge] += tmp_dict[edge][dep_edge]
+    except:
+        print(tmp_dict)
+        for edge in OMCS_edges:
+            for dep_edge in tmp_dict[edge]:
+                if dep_edge not in final_dict[edge]:
+                    final_dict[edge] = 0
+                final_dict[edge][dep_edge] += tmp_dict[edge][dep_edge]
+
+with open('OMCS-coverage-dict.json', 'w') as f:
+    json.dump(final_dict, f)
+
+
+test_result = find_OMCS_match_for_a_coreference_pair(example_and_ids[0][0], example_and_ids[0][1])
