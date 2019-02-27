a1a5403d7de292b7b61bb307ed3f4ed9f136d1b0
hongmingzhang <hzhangal0330@gmail.com>
2019-02-27 23:01:05 +0800

    pronoun coreference

--- coref_model.py
+++ coref_model.py
@@ -789,7 +789,9 @@ class CorefModel(object):
                     # print(antecedence_to_score)
                     for i in range(len(sorted_antecedents)):
                         tmp_NP_position = int(sorted_antecedents[i])
-                        if antecedence_to_score[sorted_antecedents[i]] > 0 and -2 <word_index_to_sentence_index[top_span_starts[tmp_NP_position]]-tmp_pronoun_sentence_index <= 0:
+                        if antecedence_to_score[sorted_antecedents[i]] > 0 and verify_correct_NP_match(
+                                    [top_span_starts[tmp_NP_position], top_span_ends[tmp_NP_position]], pronoun_example['candidate_NPs'],
+                                    'cover'):
                             predict_coreference += 1
                             result_by_pronoun_type[current_pronoun_type]['predict_coreference'] += 1
                             # print([top_span_starts[tmp_NP_position], top_span_ends[tmp_NP_position]])
@@ -798,6 +800,7 @@ class CorefModel(object):
                                     'cover'):
                                 correct_predict_coreference += 1
                                 result_by_pronoun_type[current_pronoun_type]['correct_predict_coreference'] += 1
+                        break
                 all_coreference += len(pronoun_example['correct_NPs'])
                 result_by_pronoun_type[current_pronoun_type]['all_coreference'] += len(
                         pronoun_example['correct_NPs'])
