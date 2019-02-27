c1a329b1c4916a603bd56ef9fe576c04634ee9d9
hongmingzhang <hzhangal0330@gmail.com>
2019-02-27 23:09:41 +0800

    pronoun coreference

--- Evaluate_separate_coreference_by_pronoun.py
+++ Evaluate_separate_coreference_by_pronoun.py
@@ -22,7 +22,10 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     test_data = list()
     print('Start to process data...')
-    with open('conll_data/test.pronoun.jsonlines', 'r') as f:
+    # with open('conll_data/test.pronoun.jsonlines', 'r') as f:
+    #     for line in f:
+    #         test_data.append(json.loads(line))
+    with open('test.english.middle.pronoun.jsonlines', 'r') as f:
         for line in f:
             test_data.append(json.loads(line))
 
