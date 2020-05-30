0d78c28b816563296a3f1306e17b26c2f870cd54
hongmingzhang <hzhangal0330@gmail.com>
2020-05-30 16:17:59 -0400

    filter MRNE embedding

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,19 @@ import os
 import tensorflow as tf
 import original_model as cm
 import util
+import ujson as json
+
+with open('freq_spans.json', 'r') as f:
+  frequency_entity_list = json.load(f)
+  new_list = list()
+  for tmp_s in frequency_entity_list:
+    new_list.append(' '.join(tmp_s))
+  new_list = set(new_list)
+
+test_data = list()
+with open('medical_data/test.pronoun.jsonlines', 'r') as f:
+  for line in f:
+    test_data.append(json.loads(line))
 
 if __name__ == "__main__":
   config = util.initialize_from_env()
@@ -15,4 +28,5 @@ if __name__ == "__main__":
   os.environ["CUDA_VISIBLE_DEVICES"] = "0"
   with tf.Session() as session:
     model.restore(session)
-    model.evaluate(session, official_stdout=True)
+    # model.evaluate(session, official_stdout=True)
+    model.evaluate_pronoun_by_frequency(session, test_data, new_list)
